f417d298224d5aab5568fc22490a525c3d6c14bf
LWRGitHub <loganreynoldslwr@gmail.com>
2020-06-21 20:08:24 -0700

    first add to repo

---
+++ src/components/Icon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+const Icon = (props) => {
+  <svg className={ props.isHighScore ? 'is-high-score' : null }></svg>
+};
+
+Icon.propTypes = {
+    isHighScore: PropTypes.bool
+};
+
+export default Icon;
